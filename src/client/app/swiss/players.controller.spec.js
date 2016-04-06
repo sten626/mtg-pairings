330078f1c77692d9bd86b4aad22a624583cd0b38
Steven Indzeoski <steven.indzeoski@me.com>
2016-04-06 00:04:51 -0600

    Starting working on tests.

---
+++ src/client/app/swiss/players.controller.spec.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  describe('PlayersController', function() {
+    var $controller;
+
+    beforeEach(module('mtgPairings.swiss'));
+    beforeEach(inject(function(_$controller_) {
+      $controller = _$controller_;
+    }));
+  });
+})();
